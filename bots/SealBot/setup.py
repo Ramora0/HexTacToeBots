67678f3d111cc49e9ea0bbdb0e0ce5f2d8a3d42d
Ramora0 <99ilovecats99@gmail.com>
2026-03-27 21:28:00 -0400

    windows support

--- bots/SealBot/setup.py
+++ bots/SealBot/setup.py
@@ -1,17 +1,23 @@
 """Build the minimax C++ extension.
 
-Usage:  cd bots/minimax && python setup.py build_ext --inplace
+Usage:  cd bots/SealBot && python setup.py build_ext --inplace
 """
 
+import platform
 from pybind11.setup_helpers import Pybind11Extension, build_ext
 from setuptools import setup
+
+if platform.system() == "Windows":
+    extra_compile_args = ["/O2", "/DNDEBUG"]
+else:
+    extra_compile_args = ["-O3", "-march=native", "-DNDEBUG"]
 
 setup(
     name="minimax_cpp",
     ext_modules=[
         Pybind11Extension("minimax_cpp", ["minimax_bot.cpp"],
                           cxx_std=17,
-                          extra_compile_args=["-O3", "-march=native", "-DNDEBUG"],
+                          extra_compile_args=extra_compile_args,
                           include_dirs=["."]),
     ],
     cmdclass={"build_ext": build_ext},
